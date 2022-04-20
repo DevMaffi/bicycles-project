4216eda1d37a171e58d42982b018c4a801e37c2b
devmaffi <devmaffi550@gmail.com>
2022-04-20 10:26:45 +0300

    Implement products filters
    Applied mixitup lib functionality on the products section.
    
    Overridden mixitup active control styles into styles.sass.
    
    Added new rule to eslintrc.json file.
    
    Added mixitup@3.3.1 to dev deps.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,6 +2,18 @@
 
 // modules
 import 'bootstrap'
+import mixitup from 'mixitup'
 
 // styles
 import '@sass/styles'
+
+// Mixer init
+
+mixitup('[data-products-container]', {
+  selectors: {
+    target: '[data-products-card-container]',
+  },
+  animation: {
+    duration: 300,
+  },
+})
